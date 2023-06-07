71a96ae3c597380a556e58ad9e54dda5e08e33b1
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-06-07 09:57:04 -0500

    update

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd'
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT artist, name, release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band'
+SELECT artist, name, release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT genre FROM albums WHERE name = 'Nevermind'
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999
+SELECT artist, name, release_date AS 'Albums From the 90''s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
-SELECT * FROM albums WHERE sales < 20
+SELECT * FROM albums WHERE sales < 20;
 
 SELECT * FROM albums WHERE genre = 'Rock';
